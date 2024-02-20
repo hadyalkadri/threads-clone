cfa4019b4c47727c19a77abdb91f8b320cd6b1ed
Hady Alkadri <98332279+hadyalkadri@users.noreply.github.com>
2024-02-20 20:31:53 +0200

    Delete Server/src/config/constants.js

--- Server/src/config/constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-    port: process.env.port || 3000,
-    db_url: "mongodb+srv://hadyalkaderi:szy7Lw94JritxF7a@cluster0.eegl2xw.mongodb.net/?retryWrites=true&w=majority",
-    GRAPHQL_PATH: '/graphql'
-}
